ea9895fd7a3f96559305480d8df9ae18c07d9a1b
justin-hzy <771736395@qq.com>
2024-07-12 16:13:54 +0800

    1.增加FlReOrderDtl1 2.增加FlReOrderDtl1Mapper 3.增加FlReOrderDtl1Service 4.增加FlReOrderDtl1ServiceImpl

---
+++ dms-bridge/src/main/java/com/me/modules/fl/entity/FlReOrderDtl1.java
@@ -0,0 +1,109 @@
+package com.me.modules.fl.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("formtable_main_263_dt1")
+public class FlReOrderDtl1 {
+
+    @TableField("id")
+    private Integer id;
+
+    @TableField("mainid")
+    private Integer mainid;
+
+    @TableField("hptxm")
+    private String hptxm;
+
+    @TableField("hpmc")
+    private String hpmc;
+
+    @TableField("gg")
+    private String gg;
+
+    @TableField("dw")
+    private String dw;
+
+    @TableField("lsj")
+    private String lsj;
+
+    @TableField("zk")
+    private String zk;
+
+    @TableField("ddl")
+    private Integer ddl;
+
+    @TableField("fhl")
+    private Integer fhl;
+
+    @TableField("ddje")
+    private String ddje;
+
+    @TableField("zlsje")
+    private String zlsje;
+
+    @TableField("zfhje")
+    private String zfhje;
+
+    @TableField("cb")
+    private String cb;
+
+    @TableField("bz")
+    private String bz;
+
+    @TableField("rq")
+    private String rq; // Assuming this is a date formatted as a string
+
+    @TableField("djbh")
+    private String djbh;
+
+    @TableField("khddh")
+    private String khddh;
+
+    @TableField("khbm")
+    private String khbm;
+
+    @TableField("kh")
+    private String kh;
+
+    @TableField("fhzje")
+    private String fhzje;
+
+    @TableField("hptxm1")
+    private String hptxm1;
+
+    @TableField("kthsl")
+    private Integer kthsl;
+
+    @TableField("ydhjey")
+    private String ydhjey;
+
+    @TableField("zk1")
+    private String zk1;
+
+    @TableField("xtdh")
+    private String xtdh;
+
+    @TableField("tlddhm")
+    private String tlddhm;
+
+    @TableField("zt")
+    private String zt;
+
+    @TableField("djlx")
+    private String djlx;
+
+    @TableField("ph")
+    private String ph;
+
+    @TableField("jkyy")
+    private String jkyy;
+
+    @TableField("rkrq")
+    private String rkrq;
+
+    @TableField("szzt")
+    private String szzt;
+}
