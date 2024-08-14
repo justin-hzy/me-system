42f8e51b3df88c64ed2b25bc30c1aba52389a203
justin-hzy <771736395@qq.com>
2024-08-14 14:37:15 +0800

    优化同步积分逻辑

--- nascent-bridge/src/main/java/com/me/nascent/modules/point/service/impl/TransPointServiceImpl.java
+++ nascent-bridge/src/main/java/com/me/nascent/modules/point/service/impl/TransPointServiceImpl.java
@@ -1,17 +1,22 @@
 package com.me.nascent.modules.point.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.me.nascent.common.config.NascentConfig;
-import com.me.nascent.modules.member.entity.MemberNickInfo;
-import com.me.nascent.modules.member.service.MemberNickInfoService;
+import com.me.nascent.modules.member.entity.PureMemberNickInfo;
+import com.me.nascent.modules.member.service.PureMemberNickInfoService;
+import com.me.nascent.modules.point.entity.PureMemberPoint;
+import com.me.nascent.modules.point.service.PureMemberPointService;
 import com.me.nascent.modules.point.service.TransPointService;
+import com.me.nascent.modules.token.entity.Token;
 import com.me.nascent.modules.token.service.TokenService;
 import com.nascent.ecrp.opensdk.core.executeClient.ApiClient;
 import com.nascent.ecrp.opensdk.core.executeClient.ApiClientImpl;
 import com.nascent.ecrp.opensdk.domain.customer.NickPlatform;
+import com.nascent.ecrp.opensdk.domain.point.CustomerAccountPointInfo;
 import com.nascent.ecrp.opensdk.request.point.CustomerPointInfoQueryRequest;
 import com.nascent.ecrp.opensdk.response.point.CustomerPointInfoQueryResponse;
-import com.nascent.ecrp.opensdk.response.refund.RefundInfoSynResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -29,7 +34,9 @@ public class TransPointServiceImpl implements TransPointService {
 
     private TokenService tokenService;
 
-    private MemberNickInfoService memberNickInfoService;
+    private PureMemberNickInfoService pureMemberNickInfoService;
+
+    private PureMemberPointService pureMemberPointService;
 
     @Override
     public void transPoint(String integralAccount,Long viewId) throws Exception {
@@ -43,7 +50,7 @@ public class TransPointServiceImpl implements TransPointService {
         request.setIntegralAccount(integralAccount);
         request.setViewId(viewId);
 
-        List<MemberNickInfo> memberNickInfos = memberNickInfoService.list();
+        List<PureMemberNickInfo> pureMemberNickInfos = pureMemberNickInfoService.list();
 
 
         int batchSize = 100; // 每次处理的数据量
@@ -52,15 +59,17 @@ public class TransPointServiceImpl implements TransPointService {
         int loopCount = (int) Math.ceil((double) totalSize / batchSize); // 需要循环的次数
 
         for (int i = 0; i < loopCount; i++) {
+            List<PureMemberPoint> insertMemberPoints = new ArrayList<>();
+            List<PureMemberPoint> updateMemberPoints = new ArrayList<>();
             int start = i * batchSize; // 开始索引
             int end = Math.min((i + 1) * batchSize, totalSize); // 结束索引，确保不超过总数据量
 
-            List<MemberNickInfo> batchList = memberNickInfos.subList(start,end);
+            List<PureMemberNickInfo> batchList = pureMemberNickInfos.subList(start,end);
             log.info("batchList="+batchList.toString());
 
             List<NickPlatform> nickPlatforms = new ArrayList<>();
 
-            for (MemberNickInfo obj : batchList){
+            for (PureMemberNickInfo obj : batchList){
                 NickPlatform nickPlatform = new NickPlatform();
                 BeanUtils.copyProperties(obj,nickPlatform);
                 nickPlatforms.add(nickPlatform);
@@ -71,9 +80,44 @@ public class TransPointServiceImpl implements TransPointService {
             CustomerPointInfoQueryResponse response = client.execute(request);
             log.info(response.getBody());
 
+            if("60001".equals(response.getCode())){
+                UpdateWrapper<Token> updateWrapper = new UpdateWrapper<>();
+                updateWrapper.eq("name","nascent").set("token",tokenService.getToken());
+                tokenService.update(updateWrapper);
+            }else if("200".equals(response.getCode())){
+                log.info(response.getRequestId());
+                List<CustomerAccountPointInfo> accountPointInfos = response.getResult();
+
+                for (CustomerAccountPointInfo accountPointInfo  : accountPointInfos){
+
+                    QueryWrapper<PureMemberPoint> pureMemberPointQuery = new QueryWrapper<>();
+                    pureMemberPointQuery.eq("nasOuid",accountPointInfo.getNasOuid()).eq("platform",accountPointInfo.getPlatform());
+
+                    PureMemberPoint existObj = pureMemberPointService.getOne(pureMemberPointQuery);
+
+                    PureMemberPoint memberPoint = new PureMemberPoint();
+                    BeanUtils.copyProperties(accountPointInfo,memberPoint);
+
+
+                    if(existObj!=null){
+                        UpdateWrapper<PureMemberPoint> pureMemberPointUpdate = new UpdateWrapper<>();
+                        pureMemberPointUpdate.eq("nasOuid",accountPointInfo.getNasOuid()).set("score",accountPointInfo.getScore());
+
+                        //pureMemberPointService.update(pureMemberPointUpdate);
+
+                    }else {
+                        insertMemberPoints.add(memberPoint);
+                    }
+                }
+
+                if (CollUtil.isNotEmpty(updateMemberPoints)){
+                    //pureMemberPointService.saveOrUpdateBatch(updateMemberPoints);
+                }
+
+                if (CollUtil.isNotEmpty(insertMemberPoints)){
+                    //pureMemberPointService.saveBatch(insertMemberPoints);
+                }
+            }
         }
-
-
-
     }
 }
--- nascent-bridge/src/test/java/com/me/point/PointTest.java
+++ nascent-bridge/src/test/java/com/me/point/PointTest.java
@@ -21,6 +21,6 @@ public class PointTest {
         /*pcode-206261	泊美积分体系
           pcode-206256	Za线上积分体系
           pcode-206258	Za姬芮线下积分体系*/
-        transPointService.transPoint("pcode-206261",100000387L);
+        transPointService.transPoint("pcode-206258",100000386L);
     }
 }
