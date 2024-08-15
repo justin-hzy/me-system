9b9da67c94dfa0194915326e94dfe8a32a0e610d
justin-hzy <771736395@qq.com>
2024-08-15 09:55:28 +0800

    1.优化同步订单逻辑

--- nascent-bridge/src/test/java/com/me/order/TradeTest.java
+++ nascent-bridge/src/test/java/com/me/order/TradeTest.java
@@ -28,10 +28,7 @@ import java.time.LocalDateTime;
 import java.time.Year;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @SpringBootTest
 @Slf4j
@@ -112,9 +109,55 @@ public class TradeTest {
             e.printStackTrace();
         }*/
 
-        try {
-            Date startDate = sdf.parse("2023-02-21 16:37:16");
-            Date endDate = sdf.parse("2023-02-23 17:59:59");
+        String year = "2023";
+
+        Calendar cal = Calendar.getInstance();
+
+        for (int month = 10; month <= 10; month++) {
+            cal.set(Calendar.YEAR, Integer.parseInt(year));
+            cal.set(Calendar.MONTH, month - 1);
+            cal.set(Calendar.DAY_OF_MONTH, 1);
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            Date startDate = cal.getTime();
+
+            cal.add(Calendar.MONTH, 1);
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+            cal.set(Calendar.HOUR_OF_DAY, 23);
+            cal.set(Calendar.MINUTE, 59);
+            cal.set(Calendar.SECOND, 59);
+            cal.set(Calendar.MILLISECOND, 999);
+            Date endDate = cal.getTime();
+
+            String startStr = sdf.format(startDate);
+            String endStr = sdf.format(endDate);
+            System.out.println("同步会员数据: " + startStr + " 到 " + endStr);
+
+            while (startDate.before(endDate)) {
+                Date endDateOfWeek = new Date(startDate.getTime() + 30 * 60 * 1000); // 修改为30分钟
+                if (endDateOfWeek.after(endDate)) {
+                    endDateOfWeek = endDate;
+                }
+
+                String startStr1 = sdf.format(startDate);
+                String endStr2 = sdf.format(endDateOfWeek);
+                System.out.println("同步订单数据: " + startStr1 + " 到 " + endStr2);
+
+                transOrderService.transOrder(startDate, endDateOfWeek);
+
+                startDate = endDateOfWeek; // 修改为1分钟
+                //System.out.println("下一个开启时间:" + startDate);
+            }
+
+        }
+
+
+
+        /*try {
+            Date startDate = sdf.parse("2023-12-01 00:00:00");
+            Date endDate = sdf.parse("2023-12-31 17:59:59");
 
             while (startDate.before(endDate)) {
                 Date endDateOfWeek = new Date(startDate.getTime() + 30 * 60 * 1000); // 修改为30分钟
@@ -133,7 +176,7 @@ public class TradeTest {
             }
         } catch (ParseException e) {
             e.printStackTrace();
-        }
+        }*/
 
 
 
