654fc7e2ae7aff381fe99d039074d6e3f9461ff4
justin-hzy <771736395@qq.com>
2024-08-20 17:34:07 +0800

    1.优化@Service用错地方

--- nascent-bridge/src/main/java/com/me/nascent/modules/qimen/service/QiMenCustomerExtendService.java
+++ nascent-bridge/src/main/java/com/me/nascent/modules/qimen/service/QiMenCustomerExtendService.java
@@ -4,6 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.me.nascent.modules.qimen.entity.QiMenCustomerExtend;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface QiMenCustomerExtendService extends IService<QiMenCustomerExtend> {
 }
--- nascent-bridge/src/main/java/com/me/nascent/modules/qimen/service/impl/QiMenCustomerExtendServiceImpl.java
+++ nascent-bridge/src/main/java/com/me/nascent/modules/qimen/service/impl/QiMenCustomerExtendServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.me.nascent.modules.qimen.entity.QiMenCustomerExtend;
 import com.me.nascent.modules.qimen.mapper.QiMenCustomerExtendMapper;
 import com.me.nascent.modules.qimen.service.QiMenCustomerExtendService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QiMenCustomerExtendServiceImpl extends ServiceImpl<QiMenCustomerExtendMapper, QiMenCustomerExtend> implements QiMenCustomerExtendService {
 }
