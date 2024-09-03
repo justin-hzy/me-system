b977349ad82e1a1d5764e5db6cd9392312bf5e82
justin-hzy <771736395@qq.com>
2024-09-03 15:55:44 +0800

    1.优化会员测试单元

--- nascent-bridge/src/test/java/com/me/member/MemberTest.java
+++ nascent-bridge/src/test/java/com/me/member/MemberTest.java
@@ -90,7 +90,7 @@ public class MemberTest {
 
     @Test
     public void transPureStoreMemberByRange() throws Exception {
-        String year = "2024";
+        //String year = "2024";
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Calendar cal = Calendar.getInstance();
@@ -112,30 +112,31 @@ public class MemberTest {
 
         */
         Long shopId = 100156928L;
+        for (int year = 2023;year<=2024;year++){
+            for (int month = 1; month <= 12; month++) {
+                cal.set(Calendar.YEAR, year);
+                cal.set(Calendar.MONTH, month - 1);
+                cal.set(Calendar.DAY_OF_MONTH, 1);
+                cal.set(Calendar.HOUR_OF_DAY, 0);
+                cal.set(Calendar.MINUTE, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MILLISECOND, 0);
+                Date startDate = cal.getTime();
 
-        for (int month = 1; month <= 12; month++) {
-            cal.set(Calendar.YEAR, Integer.parseInt(year));
-            cal.set(Calendar.MONTH, month - 1);
-            cal.set(Calendar.DAY_OF_MONTH, 1);
-            cal.set(Calendar.HOUR_OF_DAY, 0);
-            cal.set(Calendar.MINUTE, 0);
-            cal.set(Calendar.SECOND, 0);
-            cal.set(Calendar.MILLISECOND, 0);
-            Date startDate = cal.getTime();
+                cal.add(Calendar.MONTH, 1);
+                cal.add(Calendar.DAY_OF_MONTH, -1);
+                cal.set(Calendar.HOUR_OF_DAY, 23);
+                cal.set(Calendar.MINUTE, 59);
+                cal.set(Calendar.SECOND, 59);
+                cal.set(Calendar.MILLISECOND, 999);
+                Date endDate = cal.getTime();
 
-            cal.add(Calendar.MONTH, 1);
-            cal.add(Calendar.DAY_OF_MONTH, -1);
-            cal.set(Calendar.HOUR_OF_DAY, 23);
-            cal.set(Calendar.MINUTE, 59);
-            cal.set(Calendar.SECOND, 59);
-            cal.set(Calendar.MILLISECOND, 999);
-            Date endDate = cal.getTime();
-
-            String startStr = sdf.format(startDate);
-            String endStr = sdf.format(endDate);
-            System.out.println("同步会员数据: " + startStr + " 到 " + endStr);
-            // 在这里调用处理会员数据的方法
-            memberMigrationService.transPureStoreMemberByRange(startDate, endDate,shopId);
+                String startStr = sdf.format(startDate);
+                String endStr = sdf.format(endDate);
+                System.out.println("同步会员数据: " + startStr + " 到 " + endStr);
+                // 在这里调用处理会员数据的方法
+                memberMigrationService.transPureStoreMemberByRange(startDate, endDate,shopId);
+            }
         }
     }
 
