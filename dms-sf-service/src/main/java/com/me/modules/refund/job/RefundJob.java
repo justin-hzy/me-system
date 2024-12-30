4e1c8e7257214ff2fd19e737e0bbc4bfe672af41
justin-hzy <771736395@qq.com>
2024-12-30 14:15:17 +0800

    出入库回传定时器

---
+++ dms-sf-service/src/main/java/com/me/modules/refund/job/RefundJob.java
@@ -0,0 +1,38 @@
+package com.me.modules.refund.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.me.modules.pur.service.PurOrderService;
+import com.me.modules.refund.entity.ThRefund;
+import com.me.modules.refund.service.ThRefundService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+@AllArgsConstructor
+public class RefundJob {
+
+    private PurOrderService purOrderService;
+
+    private ThRefundService thRefundService;
+
+    @Scheduled(cron = "0 0 9-21/1 * * ?")
+    void queryRefundDtl() throws IOException {
+
+        log.info("执行退货入库订单已入库明细查询定时任务");
+        QueryWrapper<ThRefund> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("is_receive","1");
+        List<ThRefund> refunds = thRefundService.list(queryWrapper);
+
+        for (ThRefund refund : refunds){
+            purOrderService.transPurOrderDtl(refund);
+        }
+    }
+}
---
+++ dms-sf-service/src/main/java/com/me/modules/sale/job/SaleOrderJob.java
@@ -0,0 +1,40 @@
+package com.me.modules.sale.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.me.modules.sale.entity.ThSaleOrder;
+import com.me.modules.sale.service.SaleOrderService;
+import com.me.modules.sale.service.ThSaleOrderService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+@AllArgsConstructor
+public class SaleOrderJob {
+
+    private SaleOrderService service;
+
+    private ThSaleOrderService thSaleOrderService;
+
+    @Scheduled(cron = "0 0 9-21/1 * * ?")
+    void querySaleOrderDtl() throws IOException {
+
+        log.info("执行销售订单已出库明细查询定时任务");
+        QueryWrapper<ThSaleOrder> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("is_send","1");
+        List<ThSaleOrder> thSaleOrders = thSaleOrderService.list(queryWrapper);
+
+        for (ThSaleOrder thSaleOrder : thSaleOrders){
+            service.transSaleOrderDtl(thSaleOrder);
+        }
+    }
+
+
+}
